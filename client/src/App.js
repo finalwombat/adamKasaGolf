522b53677f8b3cd46040ab305f1d6d14d72536a3
Jay Kelly <comital333@gmail.com>
2017-09-22 16:48:34 +1000

    added background component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Background from './Background'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <Background></Background>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
