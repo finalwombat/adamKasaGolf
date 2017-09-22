5cd248422bbfbbe1dbe35197348d3e7ecb7b9342
Jay Kelly <comital333@gmail.com>
2017-09-22 17:18:33 +1000

    removed surrounding div from background

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <Background img={Golfer}></Background>
-        </div>
+        <Background img={Golfer}></Background>
       </div>
     );
   }
