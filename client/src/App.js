1b03583307e1159f7f989a0ba7524c4a95045d87
Jay Kelly <comital333@gmail.com>
2017-09-22 17:05:48 +1000

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Background from './Background'
-import './App.css';
+import Golfer from './img/golfer.webp'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <Background></Background>
+          <Background img={Golfer}></Background>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
