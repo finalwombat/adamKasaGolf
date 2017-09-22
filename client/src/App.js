4ec82600019f869faf3a2fcdd58fcfbff0d579c4
Jay Kelly <comital333@gmail.com>
2017-09-22 16:54:33 +1000

    removed svg and text

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Background from './Background'
 import './App.css';
 
@@ -8,13 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
           <Background></Background>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
