6896240343a58ed82001fd6f0e2967dd66a1f86b
Jay Kelly <comital333@gmail.com>
2017-10-18 13:46:59 +1000

    nothing

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-class Menu extends Component {
-  render() {
+const Menu = () => {
     return (
       <div className="menu">
-        <Link to='/'>Home</Link>
-        <Link to='/schedule'>Schedule</Link>
-        <Link to='/about'>About</Link>
+        <Link to="/">Home</Link>
+        <Link to="/schedule">Schedule</Link>
+        <Link to="/about">About</Link>
       </div>
-    );
+    )
   }
-}
 
 export default Menu
