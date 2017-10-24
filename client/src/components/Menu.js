74258140ba9a18bebdbee3253c774288f16bb119
Jay Kelly <comital333@gmail.com>
2017-10-24 13:05:47 +1000

    styling for menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import './Menu.css'
 
-const Menu = () => {
+
+
+const handleOnClink = (e) => {
+
+  console.log(e.target.parent)
+}
+
+const Menu = (props) => {
     return (
-      <div className="menu">
-        <Link to="/">Home</Link>
-        <Link to="/schedule">Schedule</Link>
-        <Link to="/about">About</Link>
-      </div>
+      <nav className="slidemenu">
+        <input type="radio" name="slideItem" id="slide-item-1" className="slide-toggle"
+        onClick={() => {props.history.push('/')}} defaultChecked/>
+        <label htmlFor="slide-item-1"><p className="icon">♬</p><span>Home</span></label>
+        <input type="radio" name="slideItem" id="slide-item-2" className="slide-toggle"
+        onClick={() => {props.history.push('/schedule')}} />
+        <label htmlFor="slide-item-2" ><p className="icon">♬</p><span>Schedule</span></label>
+        <input type="radio" name="slideItem" id="slide-item-3" className="slide-toggle"
+        onClick={() => {props.history.push('/about')}} />
+        <label htmlFor="slide-item-3"><p className="icon">♬</p><span>About</span></label>
+
+        <div className="clear"></div>
+
+
+       <div className="slider">
+         <div className="bar"></div>
+       </div>
+      </nav>
     )
   }
 
-export default Menu
+export default withRouter(Menu)
