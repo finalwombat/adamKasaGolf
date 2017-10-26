f219da7a17d63620f2cfef0a3b0c7f784d737e52
Jay Kelly <comital333@gmail.com>
2017-10-26 17:33:38 +1000

    added socail component to page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Background from '../components/Background'
 import backgroundImage from '../img/golfshot.jpg'
 import FacebookFeed from '../components/FacebookFeed'
+import Social from '../components/Social'
 
 const Home = () => {
     return (
       <div className="home">
         <Background img={backgroundImage}></Background>
         <FacebookFeed />
+        <Social />
       </div>
     );
   }
