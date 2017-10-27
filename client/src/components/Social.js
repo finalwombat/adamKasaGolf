333cc3efd98a9ae3b01675fa907c6ae6c97776c0
Jay Kelly <comital333@gmail.com>
2017-10-27 15:32:55 +1000

    added Banner component

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -2,10 +2,12 @@ import React from 'react'
 import facebookImg from '../img/facebook.png'
 import instagramImg from '../img/instagram.png'
 import twitterImg from '../img/twitter.png'
+import Banner from './Banner'
 
 const Social = () => {
   return (
       <div className="social">
+        <Banner />
         <img src={facebookImg} alt="facebook" />
         <img src={instagramImg} alt="instagram" />
         <img src={twitterImg} alt="twitter" />
