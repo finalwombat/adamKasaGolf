dcbfb5c6d00a7431d7aca1a06e265fdf543a5e1b
Jay Kelly <comital333@gmail.com>
2017-11-13 13:18:38 +1000

    import css

--- client/src/components/PageShell.js
+++ client/src/components/PageShell.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import PageShellCss from './PageShell.css'
 
 const PageShell = Page => {
   return props =>
