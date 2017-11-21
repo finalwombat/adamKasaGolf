0f03bbff04902bf15511306efb022c0c0497d43f
Jay Kelly <comital333@gmail.com>
2017-11-21 13:18:41 +1000

    initial jsx

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const Schedule = (props) => {
+  return (
+    <div className='scheduel'>
+      <div className='titles'>
+        <h2>Date</h2>
+        <h2>Event</h2>
+        <h2>Result</h2>
+      </div>
+      <div className='events'>
+      </div>
+    </div>
 
+  )
 }
 
 export default Schedule
