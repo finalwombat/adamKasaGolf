b6c8b62e53847da0e569104cd9f99805a6fa9449
Jay Kelly <comital333@gmail.com>
2017-11-22 17:32:58 +1000

    test for schedule.js "

---
+++ client/src/tests/Schedule.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Schedule from '../components/Schedule'
+import renderer from 'react-test-renderer'
+
+it('exists', () => {
+
+})
+
+it('renders a div named schedule', () => {
+  console.log(Schedule())
+})
