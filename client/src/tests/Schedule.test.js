1b3ec1e36274ad55bb8e94fce20122ee2b45eed3
Jay Kelly <comital333@gmail.com>
2017-11-27 11:49:26 +1000

    reworked test to use snapshot

--- client/src/tests/Schedule.test.js
+++ client/src/tests/Schedule.test.js
@@ -2,10 +2,9 @@ import React from 'react'
 import Schedule from '../components/Schedule'
 import renderer from 'react-test-renderer'
 
-it('exists', () => {
-
-})
-
-it('renders a div named schedule', () => {
-  console.log(Schedule())
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Schedule></Schedule>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })
