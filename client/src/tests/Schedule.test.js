1efdc6c9c8b66397f7d0a6115639976fe9702fc3
Jay Kelly <comital333@gmail.com>
2017-11-28 12:08:09 +1000

    all tests pass

--- client/src/tests/Schedule.test.js
+++ client/src/tests/Schedule.test.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import {Schedule, ScheduleEvent} from '../components/Schedule'
-import {shallow} from 'enzyme'
+import {shallow, mount} from 'enzyme'
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Schedule></Schedule>)
+    .create(<Schedule events={events}></Schedule>)
     .toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+it('returns div with three event divs', () => {
+  const wrapper = shallow(<Schedule events={events} />)
+  expect((wrapper.find('ul').children()).length).toEqual(events.length)
+
+
 })
 
 // ScheduleEvent
 it('returns div with event details', () => {
 
   const wrapper = shallow(<ScheduleEvent date='12th sep' name='Gold Coast Open' result=''/>)
-  console.log(wrapper.html())
   expect(wrapper.html()).toEqual('<div><p>12th sep</p><p>Gold Coast Open</p><p></p></div>')
 
-
 })
+
+
+
+  const events = [
+    {
+      date: '12th sep',
+      name: 'Gold Coast Open',
+      result: '5th'
+    },
+    {
+      date: '16th oct',
+      name: 'Masters',
+      result: '1st'
+    },
+    {
+      date: '20th dec',
+      name: 'Christmass challenge',
+      result: '25th'
+    }
+  ]
