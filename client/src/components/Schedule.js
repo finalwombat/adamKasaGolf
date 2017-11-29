b24ea7584c37c5792ddf55361186de179463b768
Jay Kelly <comital333@gmail.com>
2017-11-29 17:58:20 +1000

    added flex styles

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -1,18 +1,39 @@
 import React from 'react'
 
-export const Schedule = (props) => {
+const scheduleStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '50vw'
+}
 
+const titlesStyle = {
+  display: 'flex',
+  justifyContent: 'space-between'
+}
+
+const eventsStyle = {
+  display: 'flex',
+  border: '1px solid red'
+}
+
+const scheduleEventStyle = {
+  display: 'flex',
+  justifyContent: 'space-between'
+}
+
+
+export const Schedule = (props) => {
   const events = props.events.map(event => {
     return <li><ScheduleEvent date={event.date} name={event.name} result={event.result} /></li>
   })
   return (
-    <div className='scheduel'>
-      <div className='titles'>
+    <div className='scheduel' style={scheduleStyle}>
+      <div className='titles' style={titlesStyle}>
         <h2>Date</h2>
         <h2>Event</h2>
         <h2>Result</h2>
       </div>
-      <div className='events'>
+      <div className='events' style={eventsStyle}>
         <ul>
         {events}
         </ul>
@@ -24,7 +45,7 @@ export const Schedule = (props) => {
 
 export const ScheduleEvent = (props) => {
   return (
-    <div>
+    <div className="scheduleEvent" style={scheduleEventStyle}>
       <p>{props.date}</p>
       <p>{props.name}</p>
       <p>{props.result}</p>
