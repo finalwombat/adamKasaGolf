e3a98382a8ec3aef1dd7e57c0e8b27ef84b757ff
Jay Kelly <comital333@gmail.com>
2017-11-29 16:50:16 +1000

    added schedule component and imported test data

--- client/src/pages/SchedulePage.js
+++ client/src/pages/SchedulePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Background from '../components/Background'
 import backgroundImage from '../img/481.jpg'
+import { Schedule } from '../components/Schedule'
+import scheduleData from '../events.json'
 
 const SchedulePage = () => {
     return (
       <div className="schedulePage">
         <Background img={backgroundImage}></Background>
-
+        <Schedule events={scheduleData.events}/>
       </div>
     );
   }
