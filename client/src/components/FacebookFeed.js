74cf2ab4f8b2922a865f956d40de1e0e0101adec
Jay Kelly <comital333@gmail.com>
2017-12-04 21:11:31 +1000

    trying to fix inconsistent render problem - not working yet

--- client/src/components/FacebookFeed.js
+++ client/src/components/FacebookFeed.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
-const script = function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=824211277780300';
-        fjs.parentNode.insertBefore(js, fjs);
-        };
+const myScript = function(){
+  const s = document.getElementById('fb-root')
+  const js = document.createElement('script')
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=824211277780300';
+  s.parentNode.insertBefore(js, s)
+}
 
-const FacebookFeed = () => {
+
+console.log('run')
+class FacebookFeed extends React.Component {
+
+  componentDidMount(){
+    myScript()
+  }
+
+
+
+
+
+render(){
   return (
     <div className="FacebookFeed">
     <div id="fb-root"></div>
-      {script(document, 'script', 'facebook-jssdk')}
 
       <div className="fb-page" data-href="https://www.facebook.com/adamkasagolf/" data-tabs="timeline"
           data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
@@ -20,6 +30,8 @@ const FacebookFeed = () => {
           className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/adamkasagolf/">Facebook</a></blockquote></div>
     </div>
   )
+}
+
 }
 
 export default FacebookFeed
