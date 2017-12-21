c2030e72fb0932a262f82e9531a1b6e4d5deaec0
Jay Kelly <comital333@gmail.com>
2017-12-21 11:16:16 +1000

    add headings and style

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -4,6 +4,21 @@ import instagramImg from '../img/instagram.png'
 import twitterImg from '../img/twitter.png'
 import Banner from './Banner'
 
+
+const socialStyle = {
+  
+}
+
+const topTextStyle = {
+  color: 'black',
+  fontSize: '30px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  position: 'relative'
+}
+
+
 const socialimagesStyle = {
   display: 'flex',
   justifyContent: 'space-around',
@@ -14,6 +29,10 @@ const socialimagesStyle = {
 const Social = () => {
   return (
       <div className="social">
+        <div className="topText" style={topTextStyle}>
+          <h2>Living the dream </h2>
+          <h2>with a full-time golfer</h2>
+        </div>
         <Banner text="Follow Adam"/>
         <div className="socialimages" style={socialimagesStyle}>
           <img src={facebookImg} alt="facebook" />
