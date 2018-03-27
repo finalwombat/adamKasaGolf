145ecd5e4f35dd49d6cc822e5c8b8e50cfbe4e3b
Jay Kelly <comital333@gmail.com>
2018-03-27 16:21:08 +1000

    remove unused code

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -1,36 +1,5 @@
 import React from 'react'
 import './Schedule.css'
-const scheduleStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  width: '80vw',
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  position: 'relative'
-}
-
-const titlesStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  color: '#F9D41B',
-  fontSize: '28px'
-}
-
-const eventsStyle = {
-  display: 'flex',
-  border: '1px solid red'
-}
-
-const scheduleEventStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  fontSize: '24'
-}
-
-const ulStyle = {
-  listStyle: 'none'
-}
-
 
 export const Schedule = (props) => {
   const events = props.events.map(event => {
@@ -43,28 +12,5 @@ export const Schedule = (props) => {
           <tbody> { events }</tbody>
         </table>
       </div>
-    // <div className='scheduel' style={scheduleStyle}>
-    //   <div className='titles' style={titlesStyle}>
-    //     <h2>Date</h2>
-    //     <h2>Event</h2>
-    //     <h2>Result</h2>
-    //   </div>
-    //   <div className='events' style={eventsStyle}>
-    //     <ul style={ulStyle}>
-    //     {events}
-    //     </ul>
-    //   </div>
-    // </div>
-
-  )
-}
-
-export const ScheduleEvent = (props) => {
-  return (
-    <div className="scheduleEvent" style={scheduleEventStyle}>
-      <p>{props.date}</p>
-      <p>{props.name}</p>
-      <p>{props.result}</p>
-    </div>
   )
 }
