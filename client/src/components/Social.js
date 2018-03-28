75c7f480efe705edbf04e3cb2a494ab1cb0b945f
Jay Kelly <comital333@gmail.com>
2018-03-28 15:32:41 +1000

    moved inline styles to style sheet

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -3,41 +3,17 @@ import facebookImg from '../img/facebook.png'
 import instagramImg from '../img/instagram.png'
 import twitterImg from '../img/twitter.png'
 import Banner from './Banner'
-
-
-const socialStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-around',
-  allignItems: 'center'
-}
-
-const topTextStyle = {
-  color: 'black',
-  fontSize: '30px',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  position: 'relative'
-}
-
-
-const socialimagesStyle = {
-  display: 'flex',
-  justifyContent: 'space-around',
-  position: 'relative',
-  width: '250px'
-}
+import './Social.css'
 
 const Social = () => {
   return (
-      <div className="social" style={socialStyle}>
-        <div className="topText" style={topTextStyle}>
+      <div className="social">
+        <div className="topText">
           <h2>Living the dream </h2>
           <h2>with a full-time golfer</h2>
         </div>
         <Banner text="Follow Adam"/>
-        <div className="socialimages" style={socialimagesStyle}>
+        <div className="socialimages">
           <img src={facebookImg} alt="facebook" />
           <img src={instagramImg} alt="instagram" />
           <img src={twitterImg} alt="twitter" />
