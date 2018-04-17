fcc51981384afc57cf60b899767bc56f41cb87be
Jay Kelly <comital333@gmail.com>
2018-04-17 15:42:56 +1000

    new page

---
+++ client/src/pages/Partners.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Background from '../components/Background'
+import backgroundImage from '../img/clubs.jpg'
+
+const Partners = () => {
+    return (
+      <div className="about">
+        <Background img={backgroundImage}></Background>
+      </div>
+    );
+  }
+
+export default Partners
