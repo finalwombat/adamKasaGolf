825897e3ac0d98833d38dd22ad7ac7a1fd9a8d18
Jay Kelly <comital333@gmail.com>
2018-04-30 11:10:18 +1000

    removed old commented out code

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -46,34 +46,6 @@ class Menu extends Component {
           </div>
         </div>
 
-
-
-
-        // <div className="menu" style={menuStyle}>
-        //   <div className="logo" style={logoStyle}>
-        //     <img src={logo} style={logoImgStyle} alt="logo"
-        //     onClick={() => {props.history.push('/')}} />
-        //   </div>
-        //   <nav className="slidemenu">
-        //     <input type="radio" name="slideItem" id="slide-item-1" className="slide-toggle"
-        //     onClick={() => {props.history.push('/')}} defaultChecked/>
-        //     <label htmlFor="slide-item-1"><p className="icon">♬</p><span>Home</span></label>
-        //     <input type="radio" name="slideItem" id="slide-item-2" className="slide-toggle"
-        //     onClick={() => {props.history.push('/schedule')}} />
-        //     <label htmlFor="slide-item-2" ><p className="icon">♬</p><span>Schedule</span></label>
-        //     <input type="radio" name="slideItem" id="slide-item-3" className="slide-toggle"
-        //     onClick={() => {props.history.push('/about')}} />
-        //     <label htmlFor="slide-item-3"><p className="icon">♬</p><span>About</span></label>
-        //
-        //     <div className="clear"></div>
-        //
-        //
-        //    <div className="slider">
-        //      <div className="bar"></div>
-        //    </div>
-        //   </nav>
-        // </div>
-
       )
     }
 
