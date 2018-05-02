8b62e233517c727e3b747efe700ad6c15402920f
Jay Kelly <comital333@gmail.com>
2018-05-02 13:33:22 +1000

    added new partner

--- client/src/pages/PartnersPage.js
+++ client/src/pages/PartnersPage.js
@@ -3,12 +3,14 @@ import Background from '../components/Background'
 import backgroundImage from '../img/clubs.jpg'
 import tga from '../img/thegolfathlete.jpg'
 import vp from '../img/vicpark.jpg'
+import golfinfo from '../img/golfinfonetwork.png'
 
 const PartnersPage = () => {
     return (
       <div className="partners">
           <img src={vp} />
           <img src={tga} />
+          <img src={golfinfo} />
       </div>
     );
   }
