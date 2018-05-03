6a39fea552421aa5586b60119cdc0f521401b0fe
Jay Kelly <comital333@gmail.com>
2018-05-03 12:58:05 +1000

    add overlay to partners images

--- client/src/pages/PartnersPage.js
+++ client/src/pages/PartnersPage.js
@@ -5,12 +5,38 @@ import tga from '../img/thegolfathlete.jpg'
 import vp from '../img/vicpark.jpg'
 import golfinfo from '../img/golfinfonetwork.png'
 
+import './pages.css'
+
 const PartnersPage = () => {
     return (
       <div className="partners">
-          <img src={vp} />
-          <img src={tga} />
-          <img src={golfinfo} />
+        <div className='container'>
+          <img className="image" src={vp} />
+          <a href="https://victoriapark.com.au/">
+            <div className='overlay'>
+              <div className="text">Victoria Park Golf Complex</div>
+            </div>
+          </a>
+        </div>
+        <div className='container'>
+        <a href="http://www.thegolfathlete.com/">
+          <img className="image" src={tga} />
+            <div className='overlay'>
+              <div className="text">The Golf Athlete</div>
+            </div>
+          </a>
+        </div>
+        <div className='container'>
+        <a href="http://www.golfinfonetwork.com/">
+          <img className="image" src={golfinfo} />
+            <div className='overlay'>
+              <div className="text">Golf Info Network</div>
+            </div>
+         </a>
+        </div>
+          
+          
+          
       </div>
     );
   }
