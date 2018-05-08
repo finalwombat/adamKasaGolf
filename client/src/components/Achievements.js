fb87627e38778ba78d81fb136b57b55156e26a34
Jay Kelly <comital333@gmail.com>
2018-05-08 18:57:34 +1000

    add achievement div and styling

--- client/src/components/Achievements.js
+++ client/src/components/Achievements.js
@@ -3,21 +3,28 @@ import styled from 'styled-components'
 import tamworthWin from '../img/tamworthWin.jpg'
 
 const AchievementDiv = styled.div`
-                      width: 80vw;
+                      width: 90vw;
                       height: 90vh;
                       display: flex;
                       color: white;
 
+
                       & img {
-                        position: relative;
+  
                       }
                       
                       & div {
                         width: 30%;
-                        position: absolute;
-                        right: 0;
-                        bottom: 0;
-                        margin: 30px;
+                        margin: 60px;
+                        align-self: center;
+                      }
+
+                      & a {
+                        text-decoration: none;
+                        background: blue;
+                        padding: 8px;
+                        color: black;
+                        font-size: 1.3em;
                       }
 `
 
@@ -26,13 +33,12 @@ const Achievements = (props) => {
   const Achievements = props.achievements.map( achievement => {
     return (
       <AchievementDiv>
-        <img src={tamworthWin} style={{height: '100%', width: '60%'}}/>
         <div>
           <h2>{achievement.date}</h2>
           <h1>{achievement.title}</h1>
-          <a href={achievement.link}>Read</a>
+          <a href={achievement.link}>View</a>
         </div>
-        
+        <img src={tamworthWin} style={{height: '80%', width: '40%'}}/>
       </AchievementDiv>
     )
   })
