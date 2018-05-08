b9479d0f2059d3ebf1526f098388faa3e4002393
Jay Kelly <comital333@gmail.com>
2018-05-09 09:35:30 +1000

    media query for home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,24 +3,39 @@ import Background from '../components/Background'
 import backgroundImage from '../img/golfshot.jpg'
 import FacebookFeed from '../components/FacebookFeed'
 import Social from '../components/Social'
+import styled from 'styled-components'
 
-const homeStyle = {
-}
 
-const contentStyle = {
-  display: 'flex',
-  justifyContent: 'space-around',
-  paddingTop: '50px'
-}
+
+const Content = styled.div`
+                    display: flex;
+                    justify-content: space-around;
+                    padding-top: 50px;
+                
+                @media only screen and (max-width: 599px){
+                  flex-direction: column-reverse;
+                  justify-content: space-around;
+                  align-items: center;
+                  align-content: center;
+
+                  & div {
+                    margin-top: 20px;
+                    margin-bottom: 30px;
+                  }
+                }
+
+
+`
+  
 
 const HomePage = () => {
     return (
-      <div className="home" style={homeStyle}>
+      <div className="home" >
         <Background img={backgroundImage}></Background>
-        <div className='content' style={contentStyle}>
+        <Content>
           <FacebookFeed />
           <Social />
-        </div>
+        </Content>
       </div>
     );
   }
