cafc1baf97b84239c408047344cb719d85802b34
Jay Kelly <comital333@gmail.com>
2018-05-08 10:31:46 +1000

    styled components

--- client/src/pages/PartnersPage.js
+++ client/src/pages/PartnersPage.js
@@ -1,42 +1,67 @@
 import React from 'react'
+import styled from 'styled-components'
 import Background from '../components/Background'
 import backgroundImage from '../img/clubs.jpg'
 import tga from '../img/thegolfathlete.jpg'
 import vp from '../img/vicpark.jpg'
 import golfinfo from '../img/golfinfonetwork.png'
 
-import './pages.css'
+const Container = styled.div`
+                  position: relative;`
+
+const Image = styled.img`
+                opacity: 1;
+                height: auto;
+                transition: .5s ease;
+                backface-visibility: hidden;
+                
+                &:hover {
+                  opacity: 0.3;
+                }
+                `
+const Overlay = styled.div`
+                  transition: .5s ease;
+                  opacity: 0;
+                  position: absolute;
+                  top: 50%;
+                  left: 50%;
+                  transform: translate(-50%, -50%);
+                  -ms-transform: translate(-50%, -50%);
+                  text-align: center;
+
+                  &:hover {
+                    opacity: 1;
+                  }`
+
+const Text = styled.p`
+              color: white;
+              font-size: 16px;
+              padding: 16px 32px;
+`
 
 const PartnersPage = () => {
     return (
       <div className="partners">
-        <div className='container'>
-          <img className="image" src={vp} />
+        <Container>
           <a href="https://victoriapark.com.au/">
-            <div className='overlay'>
-              <div className="text">Victoria Park Golf Complex</div>
-            </div>
+            <Image src={vp} />
+            <Overlay className='overlay'>
+              <Text>Victoria Park Golf Complex</Text>
+            </Overlay>
           </a>
-        </div>
-        <div className='container'>
         <a href="http://www.thegolfathlete.com/">
-          <img className="image" src={tga} />
-            <div className='overlay'>
-              <div className="text">The Golf Athlete</div>
-            </div>
+          <Image src={tga} />
+            <Overlay className='overlay'>
+              <Text>The Golf Athlete</Text>
+            </Overlay>
           </a>
-        </div>
-        <div className='container'>
         <a href="http://www.golfinfonetwork.com/">
-          <img className="image" src={golfinfo} />
-            <div className='overlay'>
-              <div className="text">Golf Info Network</div>
-            </div>
+          <Image src={golfinfo} />
+            <Overlay>
+              <Text>Golf Info Network</Text>
+            </Overlay>
          </a>
-        </div>
-          
-          
-          
+        </Container>
       </div>
     );
   }
