9f9e9e4e1342066da754588ffbb637eac0c8f8ce
Jay Kelly <comital333@gmail.com>
2018-05-09 09:15:41 +1000

    added media query and mobile menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Menu from './components/Menu.js'
+import MenuMobile from './components/MenuMobile'
 import { Route } from 'react-router-dom'
+import Media from 'react-media'
 
 import HomePage from './pages/HomePage'
 import SchedulePage from './pages/AchievementsPage'
@@ -14,7 +16,14 @@ class App extends Component {
   render() {
     return (
         <div className="app">
-          <Menu />
+          <Media query="(max-width: 599px)">
+            {matches => 
+              matches ? (
+                <MenuMobile />
+              ): (
+                <Menu />
+              )}
+          </Media>
           <Route path="/" exact component={PageShell(HomePage)}></Route>
           <Route path="/achievements" exact component={PageShell(AchievementsPage)}></Route>
           <Route path="/partners" exact component={PageShell(PartnersPage)}></Route>
